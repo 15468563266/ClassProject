a9f0867e4e21477ca452b4e9e5d59085f2534ae3
Quititer <mym2045926637@163.com>
2019-12-02 17:22:29 +0800

    model提交

---
+++ ClassProject/src/main/java/com/zhiyou/model/Subject.java
@@ -0,0 +1,26 @@
+package com.zhiyou.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Subject {
+	
+	private int subject_id;
+	private String subject_name;
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ ClassProject/src/main/java/com/zhiyou/model/User.java
@@ -0,0 +1,25 @@
+package com.zhiyou.model;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+	private int id;
+	private String accounts;
+	private String phone;
+	private String password;
+	private String nickname;
+	private String sex;
+	private String birthday;
+	private String address;
+	private String imgurl;
+	private Date createtime;
+	
+	
+}
